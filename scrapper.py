ed4c3794e2431dda7a5443a3e10764313cd7e312
Nabeel Hassan <nabeelhassan17@gmail.com>
2025-01-21 13:42:13 +0500

    scrapper v1.1

--- scrapper.py
+++ scrapper.py
@@ -11,15 +11,13 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 
-service = Service("C:\\Users\\sharj\\Documents\\Projects\\Data-Scraping\\NBA-stats\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe")
+service = Service("/home/tk-lpt-0809/Documents/NBA-data-scraper-scrapper_init/chromedriver-linux64/chromedriver")
 driver = webdriver.Chrome(service=service, options=options)
 
 
-url = "https://www.nba.com/players"
-driver.get(url)
-
+# url = "https://www.nba.com/players"
+# driver.get(url)
 # Find the script tag with the id "__NEXT_DATA__"
-
 # Open the webpage
 def get_player_data(driver,id,slug_name):
     data = []
@@ -47,8 +45,89 @@ def get_player_data(driver,id,slug_name):
 
 
 
+def get_match_data(driver):
+    url = "https://www.nba.com/games?date=2025-01-19"  # Replace with your actual endpoint
+    driver.get(url)
+    script = driver.find_element(By.ID, "__NEXT_DATA__")
+    
+    script_content = script.get_attribute('innerHTML')
 
+    # Parse the JSON content
+    data = json.loads(script_content)
+
+    # Now you can access the data like a regular Python dictionary
+    # print(data['props']['pageProps']['players'])
+    cards = [data['props']['pageProps']['gameCardFeed']['modules'][0]['cards'][0]]
+    matches = []
+    print("have match data")
+    for card in cards:
+        match = {}
+        match['extracted_content'] = card['cardData']
+        match['id'] = card['cardData']['gameId']
+        actions = card['cardData']['actions']
+        for action in actions:
+            if action['title'] == 'Box Score':
+                match['box_score_link'] = action['resourceLocator']['resourceUrl']
+                break
+        
+        match['players'] = get_match_player_data(driver, "https://www.nba.com/" + match['box_score_link'])
+        print("have player data")
+        matches.append(match)
+            
+            
+
+    # print(matches)
+    post_url = 'https://thankfully-brief-cat.ngrok-free.app/nba-data/players/match-stats'
+    headers = {
+        "Content-Type": "application/json"
+    }
+    
+    body = {"data": json.dumps(matches)}
+    post_response = requests.post(post_url, headers=headers, json=body)
+    if post_response.status_code == 200:
+        print("Data posted successfully")
+    else:
+        print(f"Failed to post data: {post_response.status_code}")
+    #print(data['props']['pageProps']['gameCardFeed']['modules'][0]['cards'][0]['cardData']['actions'].keys())
+    # for player in data['props']['pageProps']['players']:
+    #     print(player['PLAYER_SLUG'])
+    #     get_player_data(driver, player['PERSON_ID'], player['PLAYER_SLUG'])
+    #     headers = {
+    #         "Content-Type": "application/json"
+    #     }
+        
+    #     player_data = get_player_data(driver, player['PERSON_ID'], player['PLAYER_SLUG'])
+    #     print(player_data)
 # get_player_data(driver, 1628378, "donovan-mitchell")
+
+
+def get_match_player_data(driver, url):
+    driver.get(url)
+    script = driver.find_element(By.ID, "__NEXT_DATA__")
+
+    # Get the content of the script tag (which is typically JSON)
+    script_content = script.get_attribute('innerHTML')
+
+    # Parse the JSON content
+    data = json.loads(script_content)
+
+    # Now you can access the data like a regular Python dictionary
+    # print(data['props']['pageProps']['players'])
+    #url = "http://example.com/api/players"  # Replace with your actual endpoint
+    # data['props']['pageProps']['players']
+    return(data['props']['pageProps']['game'])
+    
+    # for player in data['props']['pageProps']['players']:
+    #     print(player['PLAYER_SLUG'])
+    #     get_player_data(driver, player['PERSON_ID'], player['PLAYER_SLUG'])
+    #     headers = {
+    #         "Content-Type": "application/json"
+    #     }
+        
+    #     player_data = get_player_data(driver, player['PERSON_ID'], player['PLAYER_SLUG'])
+    #     print(player_data)
+    
+
 def get_players(driver):
     script = driver.find_element(By.ID, "__NEXT_DATA__")
 
@@ -72,8 +151,11 @@ def get_players(driver):
         player_data = get_player_data(driver, player['PERSON_ID'], player['PLAYER_SLUG'])
         print(player_data)
 
+# get_match_players_data(driver, "https://www.nba.com/game/det-vs-hou-0022400600/box-score")
 
-# get_players(driver)
+get_match_data(driver)
 
-# # Close the driver
-requests.get('https://sportsdataapi.onrender.com/nba/players/stats/20002712?season=2024')
+# # # Close the driver
+
+
+# response = requests.get('https://thankfully-brief-cat.ngrok-free.app/nba/players/stats/20002712?season=2024')
